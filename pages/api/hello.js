075a15c49e437e2f8e4fc68900c9ae170bea0c3a
Frank <frank.pfeift@protonmail.com>
2023-02-14 17:11:48 +0100

    added example route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,49 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {
+    ApolloClient,
+    createHttpLink,
+    gql,
+    InMemoryCache,
+} from "@apollo/client";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+const createApolloClient = () => {
+    return new ApolloClient({
+        link: createHttpLink({
+            uri: "https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-svg",
+        }),
+        cache: new InMemoryCache(),
+    });
+};
+const apolloClient = createApolloClient();
+
+const GET_AAVEGOTCHI_SVGs = gql`
+    query svg_gotchi_aavegotchi($id: String!) {
+        aavegotchi(id: $id) {
+            id
+            svg
+            left
+            right
+            back
+        }
+    }
+`;
+
+export default async function handler(req, res) {
+    const gotchiId = "4430";
+
+    try {
+        const { data } = await apolloClient.query({
+            query: GET_AAVEGOTCHI_SVGs,
+            variables: { id: gotchiId },
+        });
+
+        res.setHeader("Content-Type", "application/json")
+            .setHeader("Cache-Control", "s-maxage=3600, public")
+            .status(200)
+            .json({ data });
+    } catch (e) {
+        res.setHeader("Content-Type", "application/json")
+            .status(400)
+            .json({ error: e });
+    }
 }
