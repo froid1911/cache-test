63a77190cec58188cdda5ba5750b0e20b1476e29
Frank <frank.pfeift@protonmail.com>
2023-02-14 17:36:26 +0100

    set cache to 60seconds

--- pages/api/[gotchiId]/svg.js
+++ pages/api/[gotchiId]/svg.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
 
         return res
             .setHeader("Content-Type", "application/json")
-            .setHeader("Cache-Control", "s-maxage=3600, public")
+            .setHeader("Cache-Control", "s-maxage=60, public")
             .status(200)
             .json({ data });
     } catch (e) {
